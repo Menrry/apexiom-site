8ed06f970f367a596c4ca78809b13627cb646f7e
Menrry <83178947+Menrry@users.noreply.github.com>
2026-09-09 19:19:41 -0300

    Enhance navigation with active link highlighting
    Added functionality to highlight active navigation links.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navMenu = document.querySelector(".nav-menu");
 
+  
+// Selecciona todos los enlaces dentro de la lista de navegación
+  const navLinks = document.querySelectorAll('.nav-menu a'); 
+
+  navLinks.forEach(link => {
+  link.addEventListener('click', function() {
+    // 1. Quita la clase 'active' de todos los enlaces del menú
+    navLinks.forEach(item => item.classList.remove('active'));
+    
+    // 2. Agrega la clase 'active' únicamente al enlace que recibió el clic
+    this.classList.add('active');
+  });
+});
+
+
+  
+
   if (hamburger && navMenu) {
     hamburger.addEventListener("click", () => {
       hamburger.classList.toggle("active");
